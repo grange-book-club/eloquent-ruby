85181eb5aa19be992ed5e0bf70e99ba2b16866ed
Brian Lees <blees1156@gmail.com>
2012-03-19 22:58:17 -0400

    chapter 15 code files

---
+++ chapter_15/lib/module_print_example.rb
@@ -0,0 +1,9 @@
+module ModulePrintExample
+  def print_method
+    "This is just a module"
+  end
+
+  def self.print_module_method
+    "This is a module method"
+  end
+end
---
+++ chapter_15/spec/module_print_example_spec.rb
@@ -0,0 +1,37 @@
+require 'module_print_example'
+
+class AttachToThis
+  include ModulePrintExample
+end
+
+describe ModulePrintExample do
+  it 'should use method from a module once included in' do
+    test_class = AttachToThis.new
+    test_class.print_method.should == "This is just a module"
+  end
+
+  it 'should not use a module method from a module once included' do
+    test_class = AttachToThis.new
+    message = ""
+    begin
+      test_class.print_module_method
+    rescue NoMethodError
+      message = "No Method error"
+    end
+    message.should == "No Method error"
+  end
+
+  it 'should not use a method without it being included into a class' do
+    message = ""
+    begin
+      ModulePrintExample::print_method
+    rescue NoMethodError
+      message = "No Method error"
+    end
+    message.should == "No Method error"
+  end
+
+  it 'should use a module method without it being included into a class' do
+    ModulePrintExample::print_module_method.should == 'This is a module method'
+  end
+end
