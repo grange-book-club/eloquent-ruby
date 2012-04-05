b0b0b0f83445dec832aef6517ced176c791a677b
Ashton <a_freidenberger@hotmail.com>
2012-04-05 18:02:08 -0400

    adding chapter 19 code

---
+++ chapter_19/location_class.rb
@@ -0,0 +1,12 @@
+module Location
+  def at (location, &action)
+    case location
+      when "location1" then $fuel_cost = 1
+      when "location2" then $fuel_cost = 2
+      when "location3" then $fuel_cost = 3
+      else raise "that location doesn't exist!"
+    end
+    action.call
+  end
+end
+
